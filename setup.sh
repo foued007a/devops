7c8318bdf82d8692f69f763c2fb2156be78eeaf8
foued007a <fouedgrfoufou@gmail.com>
2026-05-30 02:34:13 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "updating system"
+sudo apt update && sudo apt autoremove -y
+
+echo "installing nginx"
+sudo apt install nginx -y
+
+echo "configuration permission"
+chmod 700 ~/deploy_app
